b30ea4ae27ee9e9dc2d325d94f06f1634e97216f
Dhiraj Khadka <89305722+DKhadka319@users.noreply.github.com>
2022-02-12 08:02:42 +1100

    Create index.js

---
+++ HTML Basic Table/index.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>HTML Challenge</title>
+  </head>
+  <body>
+    <table>
+      <tr>
+        <th>FirstName</th>
+        <th>LastName</th>
+        <th>Age</th>
+      </tr>
+      <tr>
+        <td>some</td>
+        <td>random</td>
+        <td>30</td>
+      </tr>
+      <tr>
+        <td>some2</td>
+        <td>random2</td>
+        <td>40</td>
+      </tr>
+    </table>
+  </body>
+</html>

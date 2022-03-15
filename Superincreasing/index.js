cb0198d065d75ad7f2449b67a6cc33c3a6f75d28
Dhiraj Khadka <89305722+DKhadka319@users.noreply.github.com>
2022-03-15 20:32:03 +1100

    Create index.js

---
+++ Superincreasing/index.js
@@ -0,0 +1,16 @@
+
+function Superincreasing(arr) { 
+
+  var sum = 0;
+  
+  for (var i = 0; i < arr.length; i++) {
+
+    if (arr[i] <= sum) { return "false"; }
+    sum += arr[i];
+
+  }
+  
+  return true;
+         
+}
+

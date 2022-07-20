5f1fee9a98c9d1349197a7a5ebc9924250269054
Dhiraj Khadka <89305722+DKhadka319@users.noreply.github.com>
2022-07-20 23:34:17 +1000

    Create index.js

---
+++ Largest Four/index.js
@@ -0,0 +1,8 @@
+
+function LargestFour(arr) { 
+  let sorted =  arr.sort((a,b) => a - b).reverse().splice(0,4).reduce((c,d) => c+=d);
+  return sorted;
+}
+   
+// keep this function call here 
+LargestFour(readline());
